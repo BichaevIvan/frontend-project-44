708e9f08239d9979b1458f6e91001145a106ef3b
BichaevIvan <sweet22814882@gmail.com>
2023-02-09 12:38:16 +0300

    correct

--- src/cli.js
+++ src/cli.js
@@ -3,7 +3,7 @@ const tekst = (tekct) => {
 console.log ("Welcome to the Brain Games!")
 const name = readlineSync.question('May I have your name?');
 console.log (`Hello, ${name}!`);
-if (tekct !== undefined) console.log(text);
+if (tekct !== undefined) console.log(tekct);
 return name;
 };
 export default tekst;
