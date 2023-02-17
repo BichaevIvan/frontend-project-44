6306e0f9e08c8d5269f5593de151d98e396de709
BichaevIvan <sweet22814882@gmail.com>
2023-02-17 13:09:22 +0300

    correct

--- src/games/calc.js
+++ src/games/calc.js
@@ -5,7 +5,7 @@ import { answerForm, checkAnswer } from "../index.js";
 import { calculator, magicNumbers, getOperator } from "../math.js";
 
 const calc = () => {
-    const userName = tekst(`Welcome is the result of the expression?`);
+    const userName = tekst(`What is the result of the expression?`);
     for (let i = 0; i < 3; i += 1) {
         const operator = getOperator(['+','-','*']);
         const x = magicNumbers(1, 20);
