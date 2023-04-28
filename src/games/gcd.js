c4f00883fd5bbe9674e45c9eed5cc361f8c04cc6
BichaevIvan <sweet22814882@gmail.com>
2023-04-28 12:28:03 +0300

    fix

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -3,27 +3,19 @@
 import basisGames from '../index.js';
 import getRandomInt from '../getRandomInt.js';
 
-const gcd = () => {
+const getGcd= (number1, number2) => (!number2 ? number1 : getGcd(number2, number1 % number2))
+
   const noteToGcd = 'Find the greatest common divisor of given numbers.';
 
-  const taskGcd = () => {
+  const game = () => {
     const number1 = getRandomInt(100);
     const number2 = getRandomInt(100);
 
     const question = `${number1} ${number2}`;
+    const answer = getGcd(number1,number2).toString();
 
-    const getGcd = (num1, num2) => {
-      if (num1 % num2 === 0) { return num2; }
-
-      return getGcd(num2, num1 % num2);
-    };
-
-    const result = getGcd(number1, number2).toString();
-
-    return [question, result];
+    return [question, answer];
   };
+  const start = () => basisGames(noteToGcd, game);
 
-  basisGames(noteToGcd, taskGcd);
-};
-
-export default gcd;
+export default start
